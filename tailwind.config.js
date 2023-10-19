d0cd33958b8f7e7cb8d98e8923defdc060408914
SinthiaGonzalez <gonzalez.sinthia@icloud.com>
2023-10-20 01:27:28 +0200

    archivos tailwind modificados

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html"],
+  content: ["./src/**/*.svelte", "./public/index.html"],
   theme: {
     extend: {},
   },
